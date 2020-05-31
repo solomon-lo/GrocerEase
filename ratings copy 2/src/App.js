b1e6c6c385a31b2ae33bd6416b74722c95c7c502
ejychen <64817061+ejychen@users.noreply.github.com>
2020-05-30 20:03:40 -0500

    Ratings example with Okta auth
    ratings example with Okta auth

---
+++ ratings copy 2/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+import RatingPage from './pages/ratings/rating-page';
+import RatingsListPage from './pages/rating-list/rating-list';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/rating">Rate</Link>
+        <Link to="/ratinglist">My Ratings</Link>
+      </nav>
+      <main>
+        <Route exact path="/" component={()=> 'Home Page'} />
+        <SecureRoute exact path="/rating" component={RatingPage} />
+        <SecureRoute exact path="/ratinglist" component={RatingsListPage} />
+        <Route path="/implicit/callback" component={ImplicitCallback} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
