424f0620f12098412720e94851d9c6f58263445a
raklin <rak94080@hotmail.com>
2020-06-04 13:10:14 -0700

    Revert "Remove Delete Icon"
    This reverts commit 315b918dc819fda60312c5610bbb0150748dbc77.

--- src/ChatroomItem.js
+++ src/ChatroomItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import btn_icon_456038 from './images/btn_icon_456038.png';
 
 export default class ChatroomItem extends Component {
 
@@ -36,14 +37,26 @@ export default class ChatroomItem extends Component {
   
   
   onClick_elChatstore = (ev) => {
-    // Go back in screen navigation history
-    this.props.appActions.goBack();
+    let newVal = this.props.document_key;
+    this.props.appActions.updateDataSlot('ds_SlotSelectedChatroomKey', newVal);
+  
+    // Go to screen 'Messages'
+    this.props.appActions.goToScreen('messages', { transitionId: 'fadeIn' });
+  
+  
+  }
+  
+  
+  onClick_elIconButton = (ev) => {
+    // Remove row from connected sheet
+    this.props.appActions.removeFromDataSheet(this.props.dataSheetId, this.props.dataSheetRow);
   
   }
   
   
   render() {
     const value_chatname = this.props.chatroom_name;
+    const value_storename = this.props.store_name;
     
     const style_elChatname = {
       color: 'rgba(0, 0, 0, 0.8500)',
@@ -51,12 +64,40 @@ export default class ChatroomItem extends Component {
       cursor: 'pointer',
       pointerEvents: 'auto',
      };
+
+     const style_elChatname_filterOn = {
+      color: 'rgba(0, 0, 0, 0.5)',
+      textAlign: 'left',
+      cursor: 'pointer',
+      pointerEvents: 'auto',
+      opacity: '0.5'
+     }
     
-    const value_chatstore = this.props.document_key;
-    
+    const value_chatstore = this.props.store_name;
     const style_elChatstore = {
       color: 'rgba(0, 0, 0, 0.8500)',
       textAlign: 'left',
+      cursor: 'pointer',
+      pointerEvents: 'auto',
+     };
+
+    const style_elChatstore_filterOn = {
+      color: 'rgba(0, 0, 0, 0.5)',
+      textAlign: 'left',
+      cursor: 'pointer',
+      pointerEvents: 'auto',
+    };
+    
+    const style_elIconButton = {
+      display: 'block',
+      textTransform: 'uppercase',
+      backgroundImage: 'url('+btn_icon_456038+')',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '82.200%',
+      backgroundPosition: '50% 0%',
+      color: '(null)',
+      textAlign: 'left',
+      backgroundColor: 'transparent',
       cursor: 'pointer',
       pointerEvents: 'auto',
      };
@@ -68,11 +109,22 @@ export default class ChatroomItem extends Component {
       textAlign: 'left',
      };
     
-    const style_elRectangle = {
-      background: 'rgba(0, 0, 0, 0.475)',
+     const style_elText3_filterOn = {
+      color: 'rgba(0, 0, 0, 0.500)',
+      textAlign: 'left',
      };
     
-    return (
+    const style_elRectangle = {
+      background: 'rgba(0, 0, 0, 0.475)',
+    };
+
+    const style_elRectangle_filterOn = {
+      background: 'rgba(0, 0, 0, 0.475)',
+      opacity: '0.6',
+    }
+    if (value_chatstore.replace(/[^A-Za-z0-9]/g,'').toLowerCase().includes(this.props.filter.replace(/[^A-Za-z0-9]/g,'').toLowerCase()) ||
+          value_chatname.replace(/[^A-Za-z0-9]/g,'').toLowerCase().includes(this.props.filter.replace(/[^A-Za-z0-9]/g,'').toLowerCase())) {
+      return (
       <div className="ChatroomItem appBg">
         <div className="layoutFlow">
           <div className="elChatname">
@@ -85,6 +137,10 @@ export default class ChatroomItem extends Component {
             <div className="baseFont" style={style_elChatstore} onClick={this.onClick_elChatstore} >
               <div>{value_chatstore !== undefined ? value_chatstore : (<span className="propValueMissing">{this.props.locStrings.chatroomitem_text2_809202}</span>)}</div>
             </div>
+          </div>
+          
+          <div className="elIconButton">
+            <button className="actionFont" style={style_elIconButton} onClick={this.onClick_elIconButton}  />
           </div>
           
           <div className="elText3">
@@ -100,6 +156,42 @@ export default class ChatroomItem extends Component {
         
       </div>
     )
+    }
+    else {
+      return (
+        <div className="ChatroomItem appBg">
+          <div className="layoutFlow">
+            <div className="elChatname">
+              <div className="headlineFont" style={style_elChatname_filterOn} onClick={this.onClick_elChatname} >
+                <div>{value_chatname !== undefined ? value_chatname : (<span className="propValueMissing">{this.props.locStrings.chatroomitem_text_699949}</span>)}</div>
+              </div>
+            </div>
+            
+            <div className="elChatstore">
+              <div className="baseFont" style={style_elChatstore_filterOn} onClick={this.onClick_elChatstore} >
+                <div>{value_chatstore !== undefined ? value_chatstore : (<span className="propValueMissing">{this.props.locStrings.chatroomitem_text2_809202}</span>)}</div>
+              </div>
+            </div>
+            
+            <div className="elIconButton">
+              <button className="actionFont" style={style_elIconButton} onClick={this.onClick_elIconButton}  />
+            </div>
+            
+            <div className="elText3">
+              <div className="baseFont" style={style_elText3_filterOn}>
+                <div>{value_text3 !== undefined ? value_text3 : (<span className="propValueMissing">{this.props.locStrings.chatroomitem_text3_643611}</span>)}</div>
+              </div>
+            </div>
+            
+            <div className="elRectangle">
+              <div style={style_elRectangle_filterOn} />
+            </div>
+          </div>
+          
+        </div>
+      )
+
+    }
   }
   
 }
